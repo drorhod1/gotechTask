b234b7735628805d1102d25bc100e6f1360e0909
Dror Hod <drorhod1@gmail.com>
2024-05-19 14:34:13 +0300

    made inserted functions inside google class

--- pageObjectsModel/google.pages.ts
+++ pageObjectsModel/google.pages.ts
@@ -1,21 +1,32 @@
+import { Page } from "@playwright/test";
+
 class Google {
-    get googleHomePage():string {
-        return 'https://www.google.co.il/';
-    }
-    get googleSearchLine():string{
-        return 'textarea.gLFyf';
-    }
-    get googleSearchButton():string{
-        return 'div.FPdoLc.lJ9FBc input[name="btnK"]';
-    }
-    get googleSearchAllTab():string{
-        return 'div[jsname="bVqjv"]';
-    }
-    get textToSearch():string{
-        return 'RailIL';
-    }
-    get searchText():string{
-        return 'רכבת ישראל';
-    }
+  constructor() {}
+  get googleHomePage(): string {
+    return "https://www.google.co.il/";
+  }
+  get googleSearchLine(): string {
+    return "textarea.gLFyf";
+  }
+  get googleSearchButton(): string {
+    return 'div.FPdoLc.lJ9FBc input[name="btnK"]';
+  }
+  get googleSearchAllTab(): string {
+    return 'div[jsname="bVqjv"]';
+  }
+  get textToSearch(): string {
+    return "RailIL";
+  }
+  get searchText(): string {
+    return "רכבת ישראל";
+  }
+  async searchForText(page: Page, text: string) {
+    const textareaElement = await page.waitForSelector(this.googleSearchLine);
+    await textareaElement.fill(text);
+  }
+  async clickOnSearchButton(page: Page) {
+    const searchButton = await page.waitForSelector(this.googleSearchButton);
+    await searchButton.click();
+  }
 }
 export default new Google();
--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -1,17 +1,15 @@
-import { test, expect } from '@playwright/test';
-import Google from '../pageObjectsModel/google.pages';
+import { test, expect } from "@playwright/test";
+import Google from "../pageObjectsModel/google.pages";
 
-test('Google home page load', async ({ page }) => {
+test("Google home page load", async ({ page }) => {
   await page.goto(Google.googleHomePage);
   await expect(page).toHaveTitle(/Google/);
 });
 
-test('Search for RailIL', async ({ page }) => {
+test("Search for RailIL", async ({ page }) => {
   await page.goto(Google.googleHomePage);
-  const textareaElement = await page.waitForSelector(Google.googleSearchLine);
-  const searchButton = await page.waitForSelector(Google.googleSearchButton);
-  textareaElement.fill(Google.textToSearch);
-  searchButton.click();
+  Google.searchForText(page, Google.textToSearch);
+  Google.clickOnSearchButton(page);
   await page.waitForSelector(Google.googleSearchAllTab);
-  await expect(page.textContent('html')).resolves.toContain(Google.searchText);
+  await expect(page.textContent("html")).resolves.toContain(Google.searchText);
 });
