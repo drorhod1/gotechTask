12fe791160b15e50e6760f69356d0ae0d11b137f
Dror Hod <drorhod1@gmail.com>
2024-05-19 18:21:27 +0300

    more gentle way for searching

--- pageObjectsModel/wikipedia.pages.ts
+++ pageObjectsModel/wikipedia.pages.ts
@@ -2,5 +2,8 @@ class Wikipedia {
   get wikipediaHomePage() {
     return "https://www.wikipedia.org/";
   }
+  get searchLable() {
+    return "Search Wikipedia";
+  }
 }
 export default new Wikipedia();
--- tests/wikipedia.spec.ts
+++ tests/wikipedia.spec.ts
@@ -1,26 +1,16 @@
 import { test, expect } from "@playwright/test";
-import wikipedia from "../pageObjectsModel/wikipedia.pages";
+import Wikipedia from "../pageObjectsModel/wikipedia.pages";
 
-test("Wikipedia home page load", async ({ page }) => {
-  await page.goto(wikipedia.wikipediaHomePage);
-  await expect(page).toHaveTitle(/Wikipedia/);
-});
-
-test("Wikipedia search with redirection check and click", async ({ page }) => {
-  await page.goto(wikipedia.wikipediaHomePage);
-
-  const input = await page.$("#searchInput");
-  await input.fill("Pokemon");
-  console.log("Filled search input with 'Pokemon'");
-
-  const button = await page.$('#search-form button[type="submit"]');
-  await button.click();
-  console.log("Clicked search button");
-
-  await page.waitForNavigation();
-
-  console.log("No redirection occurred");
-  const pageTitle = await page.title();
-  console.log("Page title after search:", pageTitle);
+test("test", async ({ page }) => {
+  await page.goto(Wikipedia.wikipediaHomePage);
+  await page.getByLabel(Wikipedia.searchLable).click();
+  await page.getByLabel(Wikipedia.searchLable).fill("Pokemon");
+  await page.getByLabel(Wikipedia.searchLable).press("Enter");
+  await page
+    .getByRole("heading", { name: "Pokémon", exact: true })
+    .locator("i")
+    .click({
+      button: "right",
+    });
   await expect(page).toHaveTitle(/Pokémon/);
 });
